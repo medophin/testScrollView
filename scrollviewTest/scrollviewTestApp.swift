700a4472ef9cfb96232c59987962c3378729fb2c
joseph.tu <joseph.tu@cycraft.com>
2022-04-15 12:55:56 +0800

    Initial Commit

---
+++ scrollviewTest/scrollviewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrollviewTestApp.swift
+//  scrollviewTest
+//
+//  Created by dophin on 2022/4/15.
+//
+
+import SwiftUI
+
+@main
+struct scrollviewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
